24b1750d3722732183ff0214015341f2334dff16
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-05-05 12:56:36 +0530

    IMDB first 100 movies scrapped using crawl spider. Adding support for pagination.

--- imdb/imdb/spiders/ImdbBestMovies.py
+++ imdb/imdb/spiders/ImdbBestMovies.py
@@ -7,15 +7,19 @@ from scrapy.spiders import CrawlSpider, Rule
 class ImdbbestmoviesSpider(CrawlSpider):
     name = 'ImdbBestMovies'
     allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+    start_urls = ['https://www.imdb.com/list/ls068082370/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths = (r'//h3[@class="lister-item-header"]/a')), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'title': response.xpath('.//div[@class = "title_wrapper"]/h1/text()').get(),
+            'year': response.xpath('.//div[@class = "title_wrapper"]/h1/span/a/text()').get(),
+            'director': response.xpath('.//div[@class="plot_summary "]/div[@class="credit_summary_item"]/h4[contains(text(),"Director")]/../a/text()').get(),
+            'rating': response.xpath('.//span[@itemprop="ratingValue"]/text()').get(),
+            'running_time': response.xpath('normalize-space(.//div[@class = "title_wrapper"]/div/time/text())').get(),
+            'genre_1': response.xpath('.//div[@class = "title_wrapper"]/div/a[1]/text()').get(),
+            'genre_2': response.xpath('.//div[@class = "title_wrapper"]/div/a[2]/text()').get(),
+        }
