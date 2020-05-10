8cec9c22399cf60340149234aef930f8af85740b
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-05-10 12:51:13 +0530

    Flipkart Mobile Crawler with Pagination added. Run 'scrapy crawl flipkartMobiles -o <name_of_output_file>.json' to scrape the complete list.

--- flipkart_trial/flipkart_trial/spiders/flipkartMobiles.py
+++ flipkart_trial/flipkart_trial/spiders/flipkartMobiles.py
@@ -7,15 +7,22 @@ from scrapy.spiders import CrawlSpider, Rule
 class FlipkartmobilesSpider(CrawlSpider):
     name = 'flipkartMobiles'
     allowed_domains = ['www.flipkart.com']
-    start_urls = ['http://www.flipkart.com/']
+    start_urls = ['https://www.flipkart.com/search?sid=tyy&page=1']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//div[@class="_1HmYoV _35HD7C"]/div/div/div/div/a[@class="Zhf2z-"]'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//a[@class = "_3fVaIS"]/span[contains(text(), "Next")]/..'))
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'product_name': response.xpath('//span[@class="_35KyD6"]/text()').get(),
+            'ram': response.xpath('//div[@class = "_3WHvuP"]/ul/li[contains(text(), "RAM")]/text()').get(),
+            'rom': response.xpath('//div[@class = "_3WHvuP"]/ul/li[contains(text(), "ROM")]/text()').get(),
+            'display': response.xpath('//div[@class = "_3WHvuP"]/ul/li[contains(text(), "inch") and contains(text(), "Display")]/text()').get(),
+            'battery': response.xpath('//div[@class = "_3WHvuP"]/ul/li[contains(text(), "Battery")]/text()').get(),
+            'processor': response.xpath('//div[@class = "_3WHvuP"]/ul/li[contains(text(), "Processor")]/text()').get(),
+            'sim_type': response.xpath('//td[@class="_3-wDH3 col col-3-12" and contains(text(), "SIM Type")]/../td[2]/ul/li/text()').get(),
+            'discounted_price': response.xpath('//div[@class="_1uv9Cb"]/div[@class = "_1vC4OE _3qQ9m1"]/text()').get(),
+            'actual_price': response.xpath('//div[@class="_1uv9Cb"]/div[@class = "_3auQ3N _1POkHg"]/text()[2]').get()
+        }
