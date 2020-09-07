c55b23650c8f5b9b99d106df0474b8b1bce5c02b
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-09-07 17:01:49 +0530

    Increasing Batch Upload interval.

--- NCBI _Sequences/main.py
+++ NCBI _Sequences/main.py
@@ -14,7 +14,7 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Use a service account
-cred = credentials.Certificate('config/gcp-access-key.json')
+cred = credentials.Certificate('config/credentials.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -27,7 +27,7 @@ chrome_options.add_argument("--headless")
 
 #Reading the list of URLS containing mRNA names.
 
-with open('sample_humanURL.txt', 'r') as f:
+with open('humanURL.txt', 'r') as f:
     contents = [x.strip('"') for x in f.read().strip().split('\n')][1:]
     print("Num URLS-", len(contents))
     f.close()
@@ -42,11 +42,11 @@ for idx, each in enumerate(contents):
         seqs = [ x.text for x in driver.find_elements_by_xpath('//pre')]
         if len(seqs) == 18:
             break
-    print('Broken')
 
-    if idx%2 == 0:
+    if idx%50 == 0:
         if idx != 0:
             batch.commit()
+            print("Sequences Uploaded!")
         batch = db.batch()
 
 
@@ -59,11 +59,11 @@ for idx, each in enumerate(contents):
         doc_ref = db.collection(u'mRNA').document(name)
         batch.set(doc_ref, {'seq':sequence})
 
-    print("\n",len(seqs),"\n")
+    print("\nIterating-",idx,"Sequences Uploaded-",len(seqs),"\n")
 
     #commit and end if length is odd
     if idx == len(contents)-1:
-        print("TRUE!")
         batch.commit()
+        print("Sequences Uploaded!")
 
 driver.close()
